a1a8eec380ea9622b469616c28e6562859534a39
katarzyna wawrzycka <kat.wawrzycka@gmail.com>
2024-05-27 20:19:20 +0200

    wyswietlanie tagow w task index / show

---
+++ app/src/DataFixtures/TagFixtures.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Tag fixtures.
+ */
+
+namespace App\DataFixtures;
+
+use App\Entity\Tag;
+use Doctrine\Persistence\ObjectManager;
+
+/**
+ * Class TagFixtures.
+ */
+class TagFixtures extends AbstractBaseFixtures
+{
+    /**
+     * Load data.
+     */
+    public function loadData(): void
+    {
+        for ($i = 0; $i < 10; ++$i) {
+            $tag = new Tag();
+            $tag->setTitle($this->faker->word());
+            $tag->setCreatedAt(
+                \DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+            );
+            $tag->setUpdatedAt(
+                \DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+            );
+            $this->manager->persist($tag);
+        }
+
+        $this->manager->flush();
+    }
+}
